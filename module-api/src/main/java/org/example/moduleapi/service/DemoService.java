6c2ce894dba36d9b8609129b6116fff6610d930a
Beomjin Park <bumjin@gmail.com>
2024-03-10 10:46:07 +0900

    로컬환경 베타환경 profile 분리
    spring:
      activate:
        on-profile: local
    
    gradle 빌드
     빌드 (스택트레이스 출력, 디펜던시 업데이트, 테스트 제외)
      ./gradlew clean :module-api:buildNeeded --stacktrace --info --refresh-dependencies -x test
    
     DB실행
      docker-compose up
     앱 실행
     java -Dspring.profiles.active=local -jar -DSPRING_DATASOURCE_PASSWORD=암호 -jar module-api-0.0.1-SNAPSHOT.jar
    
    데이터 저장
    curl -v -G GET "http://localhost:8080/save"
    
    gradlew clean

--- module-api/src/main/java/org/example/moduleapi/service/DemoService.java
+++ module-api/src/main/java/org/example/moduleapi/service/DemoService.java
@@ -7,17 +7,22 @@ import org.example.modulecommon.domain.Member;
 import org.example.modulecommon.enums.CodeEnum;
 import org.example.modulecommon.repositories.MemberRepository;
 import org.example.modulecommon.service.CommonDemoService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DemoService {
 
+    @Value("${spring.profiles.active}")
+    private String name;
+
     private final CommonDemoService commonDemoService;
 
     private final MemberRepository memberRepository;
 
     public String save() {
+        System.out.println("name :" + name);
         memberRepository.save(Member.builder()
                 .name(Thread.currentThread().getName()
                         ).build());
